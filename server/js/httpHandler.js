89c5862c0cca44a45dcf13cf4247971b5e8cd105
Carolyn Tran <carolyntran953@gmail.com>
2020-12-20 16:16:54 -0600

    Complete Bare Minimum Requirements

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
-const messagesQueue = require('./messageQueue')
+const messagesQueue = require('./messageQueue');
 
 // Path for the background image ///////////////////////
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
@@ -13,25 +13,26 @@ module.exports.initialize = (queue) => {
   messageQueue = queue;
 };
 
-var getRandomDirection = () => {
-  var num = Math.floor(Math.random()*(3-0+1)+0);
-  var directions = ['left','right','down','up']
-  return directions[num]
-}
+const getRandomDirection = () => {
+  let num = Math.floor(Math.random()*(3-0+1)+0);
+  const directions = ['left','right','down','up'];
+  return directions[num];
+};
 
 module.exports.router = (req, res, next = ()=>{}) => {
 
-  //console.log('Serving request type ' + req.method + ' for url ' + req.url)
+  console.log('Serving request type ' + req.method + ' for url ' + req.url);
 
   if (req.method === 'OPTIONS') {
-      res.writeHead(200, headers);
-      res.end()
-      next()
-    }
+    res.writeHead(200, headers);
+    res.end();
+    next();
+  }
 
   if (req.method === 'GET') {
     if (req.url === '/') {
       res.writeHead(200, headers);
+      // res.end(messagesQueue.dequeue());
       var directionInQueue = messagesQueue.dequeue()
       if (directionInQueue) { res.end(directionInQueue) }
       res.end();
@@ -51,18 +52,20 @@ module.exports.router = (req, res, next = ()=>{}) => {
   }
 
   if (req.method === 'POST' && req.url === '/background.jpg') {
-    var fileData = Buffer.alloc(0)
+    var fileData = Buffer.alloc(0);
+
     req.on('data', (chunk) => {
-      fileData = Buffer.concat([fileData, chunk])
-    })
+      fileData = Buffer.concat([fileData, chunk]);
+    });
+
     req.on('end', () => {
-      fs.writeFile(module.exports.backgroundImageFile, fileData, (err) => {
-        res.writeHead( err ? 400 : 201, headers);
+      var file = multipart.getFile(fileData);
+      fs.writeFile(module.exports.backgroundImageFile, file.data, (err) => {
+        res.writeHead(err ? 400 : 201, headers);
         res.end();
         next();
       })
-
-    })
+    });
   }
   // invoke next() at the end of a request to help with testing!
 };
